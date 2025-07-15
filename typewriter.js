93aee644ec186bf362561b1ca3bb8a3553b40f3c
Ooxye29 <ricoerfan1@gmail.com>
2025-07-16 06:28:32 +0700

    Create typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,18 @@
+// Optional Typewriter effect (aktif di CSS pakai .typewriter)
+// Efek utama diatur lewat class di style.css
+// Kalau mau dikembangkan ke animasi huruf per huruf, bisa pakai script di bawah:
+
+// function typeWriterEffect(element, text, delay = 30) {
+//   let i = 0;
+//   function type() {
+//     if (i < text.length) {
+//       element.innerHTML += text.charAt(i);
+//       i++;
+//       setTimeout(type, delay);
+//     }
+//   }
+//   element.innerHTML = '';
+//   type();
+// }
+
+// // Replace showText() in script.js with this if full typewriter effect is needed
